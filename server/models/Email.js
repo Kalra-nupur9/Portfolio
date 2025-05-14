bff27782c84c4a4af12b1b53118564ae45b0de46
Chahatkalra29 <kalranupur6@gmail.com>
2025-05-14 18:27:19 +0530

    VITE_API_BASE_URL added

--- server/models/Email.js
+++ server/models/Email.js
@@ -1,11 +1,10 @@
+  //
 
-// 
+  const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+  const emailSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+  });
 
-const emailSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Email', emailSchema);
+  module.exports = mongoose.model("Email", emailSchema);
--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -26,7 +26,7 @@ const CallToAction = () => {
 
 
     try {
-    const res = await fetch("https://portfolio-backend-0gej.onrender.com/submit-email", {
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/submit-email`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email }),
