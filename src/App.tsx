5320647da87abe6af16aa6df792ac1c4598a7037
kalra-nupur9 <kalranupur6@gmail.com>
2026-04-21 14:41:19 +0530

    font

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,25 @@
 import "./App.css";
-
-// import ContactMe from "./Pages/ContactMe";
 import Footer from "./components/Footer";
-
-import MyProject from "./sections/MyProject";
-import MySkills from "./sections/MySkills";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import Feedback from "./components/Feedback";
-
-// import Faqs from "./Pages/Faqs";
-// import Pricing from "./components/Pricing";
 import NotFound from "./Pages/NotFound";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-// import AddFeedback from "./Pages/AddFeedback";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-
-
     <>
-    <Router>
-      <Navbar />
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/feedback" element={<Feedback/>} />
-        {/* <Route path="/feedbackAdmin" element={<FeedbackAdmin />} /> */}
-        <Route path="/projects" element={<MyProject />} />
-        <Route path="/skills" element={<MySkills />} />
-        {/* <Route path="/faqs" element={<Faqs />} />
-        <Route path="/pricing" element={<Pricing />} /> */}
-        {/* <Route path="/contactme" element={<ContactMe />} /> */}
-        {/* <Route path="/addFeedback" element={<AddFeedback/>} /> */}
-        <Route path="*" element={<NotFound />} />
-        
-      </Routes>
-      <Footer />
-    </Router>
-     <ToastContainer position="top-center" autoClose={3000} /></>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+     
+    </>
   );
 }
 
